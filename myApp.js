7b279a29f5d20b0eae44de5be2bd4688247fff89
Guilherme Zampiron Daniel <clee.vprice@gmail.com>
2022-10-10 10:26:38 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,24 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
+const mySecret = process.env['MESSAGE_STYLE'];
 
-console.log("Hello World")
+console.log("Hello World");
+
+app.use("/public", express.static(__dirname + "/public"));
+var response = "hello json";
+if(mySecret === "uppercase"){
+  app.get("/json", (req, res) => {
+    res.json({response}.toUpperCase());
+});
+}else{
+  app.get("/json", (req, res) => {
+    res.json({response});
+});
+};
+
+
+
 
 
 
